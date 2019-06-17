c5fe217a9f654091884e91dee7cfdf9de571b1fb
Senpaistorm <austin.l@qq.com>
2019-06-17 16:03:00 -0400

    Updated project component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -14,7 +14,9 @@ export class ProjectsComponent implements OnInit {
   }
 
   projects : Project[] = [
-    {id:4, name:"Bomb Man", description:``,
+    {id:4, name:"Bomb Man", description:`Bomb Man is a WebGL based 3D multi-player game that supports up to 2-4 players. The goal of Bomb Man 
+     is to eliminate your opponent by placing your bombs in tactical positions. You can get closer to your opponent by blasting your way through 
+     the obstacles. The player side that eliminates all other players wins.`,
      primaryLink:`https://github.com/UTSCC09/project-webgl-game`, secondaryLink:`https://bombman.me`,
     icon:``},
     {id:3, name:"Course Manager", description:`A web app that can manage your courses, grades, and computes your current overall mark as well 
@@ -22,14 +24,14 @@ export class ProjectsComponent implements OnInit {
      update marks with ease. The website is written with the django framework and SQLite database.`,
      primaryLink:`https://github.com/Senpaistorm/crs-manager`, secondaryLink:`https://www.austinlx.com/coursemanager`,
      icon:``},
-    {id:2, name:"Path tracer", description:`A program in C that renders a simple scene by tracing paths of which light would take in the scene.
+    {id:2, name:"Path Tracer", description:`A program in C that renders a simple scene by tracing paths of which light would take in the scene.
      Techniques like explicit sampling, importance sampling are implemented to reduce noise in the generated scene.`,
      primaryLink:`https://github.com/Senpaistorm/Path-Tracer`, secondaryLink:``,
-     icon:``},
-    {id:1, name:"Ray tracer", description:`A program in C that renders a simple scene by tracing light rays from the center of projection using 
+     icon:`../../assets/cg_conqueror.png`},
+    {id:1, name:"Ray Tracer", description:`A program in C that renders a simple scene by tracing light rays from the center of projection using 
     the Phong model. This program demonstrates many key concepts in computer graphics such as area light sources, shadows, refraction, reflection,
      normal mapping, etc. More details on how to use the program can be found inside the repository. `,
      primaryLink:`https://github.com/Senpaistorm/Ray-Tracer`, secondaryLink:``,
-     icon:``},
+     icon:`../../assets/Final_Render.png`},
   ]
 }
